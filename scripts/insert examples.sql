8be33b4f91ab46c79f77e89c6319b48c2f847da0
Max <Max@Machine>
2012-09-29 17:02:59 +0300

    postgres works

--- scripts/insert examples.sql
+++ scripts/insert examples.sql
@@ -1,2 +1,7 @@
+-- teacher
 INSERT INTO public.teacher ("name", surname, patronymic, rank) VALUES ('Василий', 'Васильев', 'Васильевич', 'Майор'); 
+
+-- address
 INSERT INTO public.address (city, street, house, apartment) VALUES ('Витебск', 'Ленина', '14', '5'); 
+
+-- group
