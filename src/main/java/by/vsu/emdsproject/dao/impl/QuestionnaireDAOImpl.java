553f614201658bee1b563d800384b8a16267e1a4
Max <Max@Machine>
2012-10-30 22:28:06 +0300

    restored

--- src/main/java/by/vsu/emdsproject/dao/impl/QuestionnaireDAOImpl.java
+++ src/main/java/by/vsu/emdsproject/dao/impl/QuestionnaireDAOImpl.java
@@ -2,7 +2,9 @@ package by.vsu.emdsproject.dao.impl;
 
 import by.vsu.emdsproject.dao.QuestionnaireDAO;
 import by.vsu.emdsproject.model.Questionnaire;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class QuestionnaireDAOImpl extends AbstractDAOImpl<Questionnaire> implements QuestionnaireDAO {
 
     public QuestionnaireDAOImpl() {
--- src/main/webapp/WEB-INF/tags/style.tag
+++ src/main/webapp/WEB-INF/tags/style.tag
@@ -1,1 +1,1 @@
-<link rel="stylesheet" href="${pageContext.request.contextPath}/resources/css/style.css" type="text/css"></style>
+<link rel="stylesheet" href="${pageContext.request.contextPath}/resources/css/style.css" type="text/css">
