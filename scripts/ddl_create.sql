3a4bb0914700b4e8c5246c07452fe36b02f3c7a5
Anton <>
2012-11-06 23:42:58 +0300

    fix scripts

--- scripts/ddl_create.sql
+++ scripts/ddl_create.sql
@@ -19,68 +19,7 @@ create table study_group (
     title varchar (10) UNIQUE NOT NULL,
     speciality_id int NOT NULL,
     PRIMARY KEY("id_group"),
-    CONSTRAINT FK_GROUP_SPECIALITY FOREIGN KEY (speciality_id) REFERENCES speciality(id_speciality) ON DELETE SET NULL ON UPDATE CASCADE
-);
-
-create table teacher (
-    id_teacher SERIAL,
-    first_name varchar(50) NOT NULL,
-    last_name  varchar(50) NOT NULL,
-    middle_name  varchar(50) NOT NULL,
-    rank varchar(20),
-    PRIMARY KEY("id_teacher")
-);
-
-create table student (
-    id_student SERIAL,
-    first_name varchar(50) NOT NULL,
-    last_name  varchar(50) NOT NULL,
-    middle_name  varchar(50) NOT NULL,
-    birth_date date,
-    group_id int,
-    questionnaire_id int, 
-    characteristic varchar(10000),
-    service_start Date, 
-    service_end Date,
-    rank varchar(20),
-    PRIMARY KEY("id_student"),
-    CONSTRAINT FK_STUDENT_GROUP FOREIGN KEY (group_id) REFERENCES study_group(id_group) ON DELETE SET NULL ON UPDATE CASCADE
-    CONSTRAINT FK_STUDENT_QUESTIONNAIRE FOREIGN KEY (questionnaire_id) REFERENCES questionnaire(id_questionnaire_id) ON DELETE SET NULL ON UPDATE CASCADE
-);
-
-create table subject (
-    id_subject SERIAL,
-    "title" varchar(100) NOT NULL,
-    PRIMARY KEY("id_subject")
-);
-
-create table speciality_subject (
-    subject_id int NOT NULL,
-    speciality_id int NOT NULL,
-    CONSTRAINT FK_SPECIALITY FOREIGN KEY (speciality_id) REFERENCES speciality(id_speciality) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT FK_SUBJECT FOREIGN KEY (subject_id) REFERENCES subject(id_subject) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-create table course (
-    id_course SERIAL,
-    teacher_id int NOT NULL,
-    subject_id int NOT NULL,
-    group_id int NOT NULL,
-    CONSTRAINT FK_COURSE_TEACHER FOREIGN KEY (teacher_id) REFERENCES teacher(id_teacher) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT FK_COURSE_SUBJECT FOREIGN KEY (subject_id) REFERENCES subject(id_subject) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT FK_COURSE_GROUP FOREIGN KEY (group_id) REFERENCES study_group(id_group) ON DELETE CASCADE ON UPDATE CASCADE,
-    PRIMARY KEY("id_course")
-);
-
-CREATE TABLE progress (
-    id_progress SERIAL,
-    student_id int NOT NULL,
-    subject_id int NOT NULL,
-    progress_result varchar (1),
-    progress_date date NOT NULL,
-    CONSTRAINT FK_PROGRESS_SUBJECT FOREIGN KEY (subject_id) REFERENCES subject(id_subject) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT FK_PROGRESS_STUDENT FOREIGN KEY (student_id) REFERENCES student(id_student) ON DELETE CASCADE ON UPDATE CASCADE,
-    PRIMARY KEY("id_progress")
+    CONSTRAINT FK_GROUP_SPECIALITY FOREIGN KEY (speciality_id) REFERENCES speciality(id_speciality) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE questionnaire(
@@ -123,7 +62,71 @@ CREATE TABLE questionnaire(
     think_about_study varchar (100),
     think_about_duty varchar (100),
     duty_start date,
-    duty_end date
-    CONSTRAINT FK_ADDRESS FOREIGN KEY (address_id) REFERENCES address(id_address) ON DELETE SET NULL ON UPDATE CASCADE
-    CONSTRAINT FK_PARENT_ADDRESS FOREIGN KEY (parents_address_id) REFERENCES address(id_address) ON DELETE SET NULL ON UPDATE CASCADE
+    duty_end date,
+    CONSTRAINT FK_ADDRESS FOREIGN KEY (address_id) REFERENCES address(id_address) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT FK_PARENT_ADDRESS FOREIGN KEY (parents_address_id) REFERENCES address(id_address) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY("id_questionnaire")
 );
+
+create table teacher (
+    id_teacher SERIAL,
+    first_name varchar(50) NOT NULL,
+    last_name  varchar(50) NOT NULL,
+    middle_name  varchar(50) NOT NULL,
+    rank varchar(20),
+    PRIMARY KEY("id_teacher")
+);
+
+create table student (
+    id_student SERIAL,
+    first_name varchar(50) NOT NULL,
+    last_name  varchar(50) NOT NULL,
+    middle_name  varchar(50) NOT NULL,
+    birth_date date,
+    group_id int,
+    questionnaire_id int, 
+    characteristic varchar(10000),
+    service_start Date, 
+    service_end Date,
+    rank varchar(20),
+    PRIMARY KEY("id_student"),
+    CONSTRAINT FK_STUDENT_GROUP FOREIGN KEY (group_id) REFERENCES study_group(id_group) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT FK_STUDENT_QUESTIONNAIRE FOREIGN KEY (questionnaire_id) REFERENCES questionnaire(id_questionnaire) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+create table subject (
+    id_subject SERIAL,
+    "title" varchar(100) NOT NULL,
+    PRIMARY KEY("id_subject")
+);
+
+create table speciality_subject (
+    subject_id int NOT NULL,
+    speciality_id int NOT NULL,
+    CONSTRAINT FK_SPECIALITY FOREIGN KEY (speciality_id) REFERENCES speciality(id_speciality) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT FK_SUBJECT FOREIGN KEY (subject_id) REFERENCES subject(id_subject) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+create table course (
+    id_course SERIAL,
+    teacher_id int NOT NULL,
+    subject_id int NOT NULL,
+    group_id int NOT NULL,
+    CONSTRAINT FK_COURSE_TEACHER FOREIGN KEY (teacher_id) REFERENCES teacher(id_teacher) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT FK_COURSE_SUBJECT FOREIGN KEY (subject_id) REFERENCES subject(id_subject) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT FK_COURSE_GROUP FOREIGN KEY (group_id) REFERENCES study_group(id_group) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY("id_course")
+);
+
+CREATE TABLE progress (
+    id_progress SERIAL,
+    student_id int NOT NULL,
+    subject_id int NOT NULL,
+    progress_result varchar (1),
+    progress_date date NOT NULL,
+    CONSTRAINT FK_PROGRESS_SUBJECT FOREIGN KEY (subject_id) REFERENCES subject(id_subject) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT FK_PROGRESS_STUDENT FOREIGN KEY (student_id) REFERENCES student(id_student) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY("id_progress")
+);
+
+
