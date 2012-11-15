a4f42e5206d5b0009025435f9e56b5990720dd8c
Max <Max@Machine>
2012-11-16 00:13:29 +0300

    teachers

--- scripts/insert_teacher.sql
+++ scripts/insert_teacher.sql
@@ -1,16 +1,16 @@
 DELETE FROM teacher;
 
 insert into public.teacher (id_teacher, first_name, last_name, middle_name, rank) 
-values (1, 'Имя','Машеро','Отчество','подполковник');
+values (1, 'Сергей','Машеро','Аркадьевич','Подполковник');
 insert into public.teacher (id_teacher, first_name, last_name, middle_name, rank) 
-values (2, 'Имя','Медведев','Отчество','полковник');
+values (2, 'Имя','Медведев','Отчество','Полковник');
 insert into public.teacher (id_teacher, first_name, last_name, middle_name, rank) 
-values (3, 'Имя','Осмоловский','Отчество','подполковник');
+values (3, 'Имя','Осмоловский','Отчество','Подполковник');
 insert into public.teacher (id_teacher, first_name, last_name, middle_name, rank) 
-values (4, 'Имя','Бурнейко','Отчество','подполковник');
+values (4, 'Имя','Бурнейко','Отчество','Подполковник');
 insert into public.teacher (id_teacher, first_name, last_name, middle_name, rank) 
-values (5, 'Имя','Венжега','Отчество','подполковник');
+values (5, 'Имя','Венжега','Отчество','Подполковник');
 insert into public.teacher (id_teacher, first_name, last_name, middle_name, rank) 
-values (6, 'Имя','Жуков','Отчество','капитан');
+values (6, 'Имя','Жуков','Отчество','Капитан');
 insert into public.teacher (id_teacher, first_name, last_name, middle_name, rank) 
-values (7, 'Имя','Ливицвкий','Отчество','полковник');
+values (7, 'Имя','Ливицкий','Отчество','Полковник');
