f93d1b042a26e6ade3c93805c6a61e3151ca9e26
Anton <>
2012-11-16 00:07:18 +0300

    script add

---
+++ scripts/insert groups.sql
@@ -0,0 +1,18 @@
+DELETE FROM study_group;
+
+insert into public.study_group (id_group, title,specialty_id) 
+values (1, 'М1', 1);
+insert into public.study_group (id_group, title,specialty_id) 
+values (2, 'МО1', 1);
+insert into public.study_group (id_group, title,specialty_id) 
+values (3, 'С1', 2);
+insert into public.study_group (id_group, title,specialty_id) 
+values (4, 'СО1', 2);
+insert into public.study_group (id_group, title,specialty_id) 
+values (5, 'А1', 3);
+insert into public.study_group (id_group, title,specialty_id) 
+values (6, 'АО1', 3);
+insert into public.study_group (id_group, title,specialty_id) 
+values (7, 'Р1', 4);
+insert into public.study_group (id_group, title,specialty_id) 
+values (8, 'РО1', 4)
