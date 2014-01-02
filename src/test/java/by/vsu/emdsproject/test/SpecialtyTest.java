b6eb5e1399bf53888f6662c10fd69373ffe618fe
M.Vasileusky <vasileusky@gmail.com>
2014-01-02 17:45:29 +0300

    jetty merge 1

--- src/test/java/by/vsu/emdsproject/test/SpecialtyTest.java
+++ src/test/java/by/vsu/emdsproject/test/SpecialtyTest.java
@@ -2,21 +2,16 @@ package by.vsu.emdsproject.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
- * Created with IntelliJ IDEA.
- * User: Max
+ * Author: Max
  * Date: 24.03.13
- * Time: 0:02
- * To change this template use File | Settings | File Templates.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:/spring/emds-web-context.xml"})
 public class SpecialtyTest {
-
 
     @Test
     public void writeReadTest() {
