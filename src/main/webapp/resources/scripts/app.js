8e406c716a27ba1ac5e707f02aa785479b011688
Cauac <mr.garansky@gmail.com>
2014-01-20 15:07:05 +0300

    photo upload

--- src/main/webapp/resources/scripts/app.js
+++ src/main/webapp/resources/scripts/app.js
@@ -107,4 +107,23 @@ App.directive('subMenu', [ function () {
     };
 }]);
 
+App.directive("fileread", [function () {
+    return {
+        scope: {
+            fileread: "="
+        },
+        link: function (scope, element, attributes) {
+            element.bind("change", function (changeEvent) {
+                var reader = new FileReader();
+                reader.onload = function (loadEvent) {
+                    scope.$apply(function () {
+                       scope.fileread = loadEvent.target.result;
+                    });
+                }
+                reader.readAsDataURL(changeEvent.target.files[0]);
+            });
+        }
+    }
+}]);
+
 
